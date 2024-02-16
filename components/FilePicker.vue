ed9a5165ff64f2bd9063637aee621c3581bd04a8
Jyrone Parker <jyrone.parker@atlasied.com>
2024-02-16 17:52:29 +0000

    Working on FIlePicker

---
+++ components/FilePicker.vue
@@ -0,0 +1,11 @@
+<template>
+    <UInput
+      icon="i-heroicons-document-arrow-up-20-solid"
+      size="sm"
+      color="white"
+      :trailing="false"
+      placeholder="Choose Video File..."
+      type="file"
+    />
+  </template>
+  
